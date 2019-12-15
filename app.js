3ff4f9961ec7e2c51d65d681fc156278c5287ec0
Jonas Laursen <jona8690@easv365.dk>
2019-12-15 14:06:58 +0100

    Did stuff

--- app.js
+++ app.js
@@ -6,35 +6,24 @@ var desiredWeight = 50;
 var threshHold = 500;
 var mqtt = require('mqtt')
 
-
-
 firebase.initializeApp({
     credential: firebase.credential.cert(smartMailBox),
     databaseURL: "https://smart-mailbox-b85b7.firebaseio.com/"
-
-
 });
-connectToMqtt();
 
-//gets the data from the firebase raspberryPies path once
+var db = firebase.database();
+let thresholdRef = db.ref("settings/weight");
+var adaRef = firebase.database().ref("history/1572874339/weight");
 
+console.log("Subscribe to threashold value from firebase");
+thresholdRef.on("value", function(snapshot) {
+    let thresholdInGrams = snapshot.val();
+    threshHold = getValue(thresholdInGrams);
+    console.log("Threashold set at " + threshHold);
+});
 
-function getGrams(value) {
-    value = objectWeight;
-    var temp = value + (31/27);
-    var temp = temp / (44/135);
-    objectWeight = temp;
-    console.log(temp)
-    return temp;
-}
-
-function getValue(grams) {
-    grams = objectWeight;
-    var temp = (grams*44)/135;
-    var temp = temp - (31/27);
-    console.log(temp)
-    return temp;
-}
+var clientId = 'reactjs';
+var host = 'wss://mqtt.flespi.io';
 
 var options = {
     keepalive: 10,
@@ -53,68 +42,66 @@ var options = {
     username: 'FlespiToken 2v2QwADAMIqKjO3q9NLBcs5ZB9wfJ1kzmadGAH5adlLm0CBXecogsj5RgqXPf5dp',
     password: '',
     rejectUnauthorized: false
+};
+
+let client = mqtt.connect(host, options);
+
+client.on('error', function (err) {
+    console.log(err);
+    client.end();
+});
+
+client.on('connect', function () {
+    console.log('client connected:' + clientId)
+});
+
+console.log("Subscribe to MQTT sensor/weight");
+client.subscribe('sensor/weight', {qos: 0});
+
+console.log("Define MQTT Message behavior");
+client.on('message', function (topic, message, packet) {
+    console.log('Received Message:= ' + message.toString() + '\nOn topic:= ' + topic);
+    checkWithFirebase(message);
+});
+
+//gets the data from the firebase raspberryPies path once
+
+
+function getGrams(value) {
+    value = objectWeight;
+    let temp = value + (31/27);
+    temp = temp / (44/135);
+    objectWeight = temp;
+    console.log("converted value " + value + " into grams: "+ temp);
+    return temp;
 }
-    function connectToMqtt() {
-        var clientId = 'mqttjs_'
 
-        var host = 'wss://mqtt.flespi.io'
+function getValue(grams) {
+    let temp = (grams*44)/135;
+    temp = temp - (31/27);
+    console.log("converted grams: " + grams + " into value: " + temp);
+    return temp;
+}
 
-        var client = mqtt.connect(host, options)
-
-        client.on('error', function (err) {
-            console.log(err)
-            client.end()
-        })
-
-        client.on('connect', function () {
-            console.log('client connected:' + clientId)
-        })
-
-        client.subscribe('sensor/weight', {qos: 0})
-
-        client.publish('sensor/weight', 'wss secure connection demo...!', {qos: 0, retain: false})
-        client.on('message', function (topic, message, packet) {
-            console.log('Received Message:= ' + message.toString() + '\nOn topic:= ' + topic)        });
-        checkWithFirebase(message)
-
-    function checkWithFirebase(message) {
-
-
-            if (objectWeight < message) {
-                objectWeight = message;
-                console.log(objectWeight + " had the value of and " + message)
-
-                var db = firebase.database();
-                var ref = db.ref("history");
-                var adaRef = firebase.database().ref("history/1572874339/weight");
-
-                adaRef.once("value", function (snapshot) {
-                    console.log(snapshot.val());
-                    if (snapshot.val() > objectWeight) {
-                        console.log("the weight was " + snapshot.val() + " and the desired weight was " + objectWeight)
-                        getGrams();
-
-                        adaRef.set({
-                            weight: {
-                                objectWeight
-
-                            }
-
-                        })
-                    }
-                    if (snapshot.val() < desiredWeight) {
-                        console.log("the weight was " + snapshot.val() + " and the desired weight was " + desiredWeight)
-                    }
-                });
-
-            } else {
-                console.log("objectweight was less than message" + message + " the object was" + objectWeight)
-
-            }
-
-        }
-
-        client.on('close', function () {
-            console.log(clientId + ' disconnected')
-        })
+function checkWithFirebase(message) {
+    if(message == "dead") {
+        console.error("Exception from Weight");
+        return;
     }
+
+    if(message == "shutdown") {
+        console.log("Weight shut down gracefully");
+        return;
+    }
+
+    let value = parseInt(message);
+    if(value > threshHold) {
+        console.log("Send notification");
+        client.publish("notification/mail", "true")
+    } else {
+        console.log("Dont send notification");
+        client.publish("notification/mail", "false");
+    }
+
+    // TODO: Add to Firebase History database
+}
